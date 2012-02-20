8ff60d0b3206a2c522c33eb292273fcedf019893
spilliams <s@spencerenglish.com>
2012-02-19 23:30:25 -0800

    git outa here, we don't need wp

--- footer.php
+++
@@ -1,9 +0,0 @@
-	<div id="footer">
-		<p>
-			<a href="http://wordpress.org/" rel="generator">Proudly powered by WordPress</a>
-		</p>
-	</div><!--end footer-->
-</div><!--end container-->
-<?php wp_footer(); ?>
-</body>
-</html>
