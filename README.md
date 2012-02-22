74e3795a8531de6f205bc698e1634762d1715863
spilliams <s@spencerenglish.com>
2012-02-22 00:09:25 -0800

    some real copy, js navigation, grid fixes, preview images

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 This is my new website. It uses [Compass](http://compass-style.org/) and [Blueprint](http://blueprintcss.org/).
+
+###TODO
+
+- link colors on feature header, article body
+- icons!
+- preview images
+- article full images
+- obtain picture of diploma...
+- more posts...somehow
+- footer content
+- "looking for my blog" or "about me" user stories
+- put Germany photos on flickr
--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,2 +1,31 @@
 // Regular theme JavaScript goes here
-;
+$(document).ready(function(){
+  jQuery(document).ready(function(){
+    h = jQuery(location).attr('hash');
+    if (h) {
+      $(h).click()
+    }
+  })
+  
+  $(".preview").click(function(){
+    $($(this).attr('rel')).click();
+  });
+  $(".article .header").click(function(){
+    body = $(this).parent().find(".body")
+    if (body.css('display') == "none") {
+      closeAll()
+      body.slideDown('fast')
+      if ("#"+$(this)[0].id != jQuery(location).attr('hash'))
+        jQuery(location).attr('hash',$(this)[0].id)
+      scrolly($(this).offset().top);
+    } else
+      body.slideUp('fast')
+  })
+  
+});
+function scrolly(i) {
+  $("html, body").animate({scrollTop:i+"px"})
+}
+function closeAll() {
+  $(".article .body").slideUp();
+}
